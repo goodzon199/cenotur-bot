014e8ec24990f1356293a04153b101f1750d864a
goodzon199 <goodzon199@gmail.com>
2026-07-07 11:08:16 +0300

    remove Docker, clean up node_modules for Railway

--- dist/bot/keyboards.d.ts
+++
@@ -1,5 +0,0 @@
-import { Markup } from 'telegraf';
-export declare const mainKeyboard: Markup.Markup<import("telegraf/typings/core/types/typegram").ReplyKeyboardMarkup>;
-export declare function searchAgainKeyboard(): Markup.Markup<import("telegraf/typings/core/types/typegram").InlineKeyboardMarkup>;
-export declare function confirmSubscriptionKeyboard(): Markup.Markup<import("telegraf/typings/core/types/typegram").InlineKeyboardMarkup>;
-//# sourceMappingURL=keyboards.d.ts.map
